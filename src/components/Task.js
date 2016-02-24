018b4391c80ee20228d7f84e31c9973f3744dc52
Akash Bharti <akash80e@gmail.com>
2016-02-24 17:49:19 +0530

    Improved the rendering and integrated all the components here

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,6 +4,7 @@ export default class Task extends React.Component {
   static propTypes = {
     task: React.PropTypes.object.isRequired
   }
+
   render () {
     const { task } = this.props
     if (task == null) {
@@ -30,15 +31,61 @@ export default class Task extends React.Component {
         )
       }
     })
+    const next = '/task/' + task.button[1]
+    const back = '/task/' + task.button[0]
 
-    const next = '/task/' + task.next
+    const Button = task.button.map((button, idx) => {
+      if (idx===0) {
+        if (button[0]==='0') {
+          return (
+            <div key={idx}>
+              <a href={next}>
+                <button type='button' className='btn btn-default btn-lg' >Next</button>
+              </a>
+            </div>
+          )
+        } else if (button[1]==='6') {
+          return (
+            <div key={idx}>
+              <a href={back}>
+                <button type='button' className='btn btn-default btn-lg' id='backButton'>Back</button>
+              </a>
+            </div>
+          )
+        } else {
+          return (
+            <div key={idx}>
+              <a href={back}>
+                <button type='button' className='btn btn-default btn-lg' >Back</button>
+              </a>
+              <a href={next}>
+                <button type='button' className='btn btn-default btn-lg' >Next</button>
+              </a>
+            </div>
+          )
+        }
+      }
+    })
+    const Image = task.image.map((url, idx) => {
+      return (
+        <div key='idx'>
+          <img src={url} id='image'></img>
+        </div>
+        )
+    })
+
     return (
-      <div>
-        {inputText}
-        {inputs}
-        <a href={next}>
-          <button type='button' className='btn btn-default btn-lg' id='buttonLg'>Next</button>
-        </a>
+      <div className='row'>
+        <div className='col-md-4'>
+          {inputText}
+          {inputs}
+          {Button}
+        </div>
+        <div className='col-md-1'>
+        </div>
+        <div className='col-md-7'>
+          {Image}
+        </div>
       </div>
     )
   }
--- src/components/TaskManager.js
+++ src/components/TaskManager.js
@@ -2,14 +2,29 @@ import React from 'react'
 import Task from './Task'
 
 // stub to simulate a server that returns task info
+
 function fetch (taskId) {
   switch (taskId) {
     case '1':
-      return {fields: ['email'], text: ['Create a new team', 'Sign up with your e-mail address'], next: 2}
+      return {fields: ['email'],
+              text: ['Create a new team', 'Sign up with your e-mail address'],
+              button: ['0', '2'],
+              image: ['http://www.willerbyhill.co.uk/wp-content/uploads/2013/06/bir_team.png']}
     case '2':
-      return {fields: ['vanityUrl', 'telephone'], text: ['text1', 'text2'], next: 3}
+      return {fields: ['team name'],
+              text: ['What do you want to call your Team', 'Name your team after the group you are working in'],
+              button: ['1', '3'],
+              image: ['http://joshuareich.org/wp-content/uploads/2014/10/book4.jpg']}
     case '3':
-      return {fields: ['teamName']}
+      return {fields: ['domain name'],
+              text: ['What web address do you want for your team', 'This is the address you will use to sign in'],
+              button: ['2', '4'],
+              image: ['http://www.networksupport.uk.com/images/large/domain_names_1.png']}
+    case '4':
+      return {fields: ['user name'],
+              text: ['Pick a username', 'Your username should be something that your coworkers would recognize'],
+              button: ['3', '5'],
+              image: ['http://easi.cc/archive/thompson/images/objects/obj6-2.jpg']}
   }
 }
 
@@ -27,7 +42,7 @@ export default class TaskManager extends React.Component {
   render () {
     return (
       <div>
-        <Task task={this.state.task}/>
+        <Task task={this.state.task} />
       </div>
     )
   }
